ce0a1ab0d1881db509547c2a776a199842bcc10d
Chayan Mistry <chayanforyou@yahoo.com>
2023-05-08 01:26:49 +0600

    initial commit

--- mrcnn_demo/m_rcnn.py
+++ mrcnn_demo/m_rcnn.py
@@ -56,7 +56,7 @@ class CustomConfig(Config):
     IMAGES_PER_GPU = 4
 
     # Number of classes
-    NUM_CLASSES = classes_number
+    NUM_CLASSES = 2
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
